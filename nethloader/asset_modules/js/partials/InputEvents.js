fdf01583108e20d5a9c456094604c3ce5313302c
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-03-16 12:23:39 +0100

    Implemented early register view. Added on mousedown listener to the inputs.

--- nethloader/asset_modules/js/partials/InputEvents.js
+++ nethloader/asset_modules/js/partials/InputEvents.js
@@ -4,34 +4,35 @@ var inputs;
 * Find all the inputs with the 'data-type' attribute
 */
 const findInputs = () => {
-    inputs = document.querySelectorAll("[data-type]");
+  inputs = document.querySelectorAll("[data-type]");
 };
 
 /*
 * Add eventListeners for input to every input found with 'findInputs()'
 */
 const addListeners = () => {
-    for (let i = inputs.length - 1; i >= 0; i--) {
-        inputs[i].addEventListener('input', manageLabelActiveState);
-    }
+  document.addEventListener('load', manageLabelActiveState);
+  for (let i = inputs.length - 1; i >= 0; i--) {
+    inputs[i].addEventListener('input', manageLabelActiveState);
+    inputs[i].addEventListener('mousedown', manageLabelActiveState);
+  }
 }
 
 /*
 * Changes the input's label state depending of wether it has content or not by adding or removing the 'has-content' class respectively.
 */
 const manageLabelActiveState = (event) => {
-    let eventInput = event.currentTarget;
-    let labelEl = eventInput.nextElementSibling;
-    console.log(labelEl);
-    if (eventInput.value) {
-        labelEl.classList.add('has-content');
-    } else if (!eventInput.value || eventInput.value == "") {
-        labelEl.classList.remove('has-content');
-    }
+  let eventInput = event.currentTarget;
+  let labelEl = eventInput.nextElementSibling;
+  if (eventInput.value) {
+      labelEl.classList.add('has-content');
+  } else if (!eventInput.value || eventInput.value == "") {
+      labelEl.classList.remove('has-content');
+  }
 };
 
 export default {
-    findInputs,
-    addListeners,
-    manageLabelActiveState
+  findInputs,
+  addListeners,
+  manageLabelActiveState
 };
