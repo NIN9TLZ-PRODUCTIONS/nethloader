e359ef9b9de9c13fc92be7a07b86585636a37c42
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-04-16 20:07:37 +0200

    Added delete image view

--- nethloader/asset_modules/js/main.js
+++ nethloader/asset_modules/js/main.js
@@ -3,9 +3,11 @@
 import InputEvents from './partials/InputEvents'
 import DropDownEvents from './partials/DropDownEvents'
 import DialogEvents from './partials/DialogEvents'
+import AjaxEvents from './partials/AjaxEvents'
 
 (function init() {
   InputEvents.init();
   DropDownEvents.init();
   DialogEvents.init();
+  AjaxEvents.deleteImagesInit();
 })();
---
+++ nethloader/asset_modules/js/partials/AjaxEvents.js
@@ -0,0 +1,40 @@
+﻿/* 
+* Find and manage delete buttons
+*/
+const deleteConfirmButton = document.querySelector("[data-deleteconfirm]");
+
+var deleteButtons;
+
+const deleteImagesInit = () => {
+  // Find the confirm button
+  findDeleteButtons();
+  addDelButEventListeners();
+}
+
+const findDeleteButtons = () => {
+  deleteButtons = document.querySelectorAll("[data-delimageid]");
+
+}
+
+const addDelButEventListeners = () => {
+  for (let i = deleteButtons.length - 1; i >= 0; i--) {
+    deleteButtons[i].addEventListener('click', deleteImage);
+  }
+}
+
+const deleteImage = (event) => {
+  let imageId = event.currentTarget.dataset.delimageid;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", "/image/delete/?id=" + imageId, true);
+  xmlhttp.setRequestHeader('RequestVerificationToken', antiforgeryToken);
+  xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  deleteConfirmButton.addEventListener('click', () => {
+    xmlhttp.send();
+    location.reload(true);
+  });
+  deleteConfirmButton.removeEventListener('click', () => {});
+}
+
+export default {
+  deleteImagesInit
+}
