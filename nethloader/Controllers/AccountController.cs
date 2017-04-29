0278f96e784aa156575f23c8d2f8e6ffcc415d00
Claudio4 <me@claudio4.com>
2017-04-29 22:02:58 +0100

    Remove Full name and uses username for login

--- nethloader/Controllers/AccountController.cs
+++ nethloader/Controllers/AccountController.cs
@@ -70,7 +70,7 @@ namespace nethloader.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation(1, "User logged in.");
@@ -124,9 +124,8 @@ namespace nethloader.Controllers
             {
                 var user = new User()
                 {
-                    UserName = model.Email,
+                    UserName = model.UserName,
                     Email = model.Email,
-                    FullName = model.FullName,
                     RegisterDate = DateTime.Now,
                     ApiKey = GenerateApiKey()
                 };
---
+++ nethloader/Data/Migrations/20170429210219_RemoveFullName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace nethloader.Data.Migrations
+{
+    public partial class RemoveFullName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+    }
+}
