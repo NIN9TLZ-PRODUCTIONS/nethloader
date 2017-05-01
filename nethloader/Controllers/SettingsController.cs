9ce58073dfe5bdfee65fc47ce52850faeed9c0de
Claudio4 <me@claudio4.com>
2017-05-01 19:12:44 +0100

    Shows ApiKey in settings

--- nethloader/Controllers/SettingsController.cs
+++ nethloader/Controllers/SettingsController.cs
@@ -1,17 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using nethloader.Models;
 
 namespace nethloader.Controllers
 {
     [Authorize]
     public class SettingsController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+        public SettingsController(UserManager<User> userManager)
         {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ViewData["User"] = await _userManager.FindByIdAsync(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
             return View();
         }
     }
