441ae53cc9487f1d544c56e9c4504018663c5757
Guillem Arias <gariasfa7@alumnes.ub.edu>
2017-05-04 14:27:28 +0200

    Nethloader design restore point.

--- nethloader/asset_modules/js/main.js
+++ nethloader/asset_modules/js/main.js
@@ -1,5 +1,6 @@
 import InputEvents from './partials/InputEvents';
 import DropDownEvents from './partials/DropDownEvents';
+import SettingsMenuEvents from './partials/SettingsMenuEvents';
 import DialogEvents from './partials/DialogEvents';
 import AjaxEvents from './partials/AjaxEvents';
 import './partials/mdripple.js';
@@ -8,6 +9,7 @@ import './partials/mdripple.js';
   'use strict';
   InputEvents.init();
   DropDownEvents.init();
+  SettingsMenuEvents.init();
   DialogEvents.init();
   AjaxEvents.init();
 })();
